7467673a0204d8c856f8844d0854c76309bddf78
gdwivedi123 <50792346+gdwivedi123@users.noreply.github.com>
2020-07-31 11:16:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var innerhtml = this.innerHTML;
+    makeSound(innerhtml);
+    buttonClickEffect(innerhtml);
+  });
+}
+
+  document.addEventListener('keydown', function(event) {
+    makeSound(event.key);
+    buttonClickEffect(event.key);
+  });
+
+  function makeSound(input) {
+    switch (input) {
+      case "w":
+        var audio = new Audio('sounds/crash.mp3');
+        audio.play();
+        break;
+      case "a":
+        var audio = new Audio('sounds/kick-bass.mp3');
+        audio.play();
+        break;
+      case "s":
+        var audio = new Audio('sounds/snare.mp3');
+        audio.play();
+        break;
+      case "d":
+        var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
+        break;
+      case "j":
+        var audio = new Audio('sounds/tom-2.mp3');
+        audio.play();
+        break;
+      case "k":
+        var audio = new Audio('sounds/tom-3.mp3');
+        audio.play();
+        break;
+      case "l":
+        var audio = new Audio('sounds/tom-4.mp3');
+        audio.play();
+        break;
+      default:
+        alert("Invalid Input");
+    }
+  }
+
+function buttonClickEffect(key)
+{
+   var clickedButton = document.querySelector("." + key);
+   clickedButton.classList.add("pressed");
+   setTimeout(function()
+   {
+     clickedButton.classList.remove("pressed");
+   }, 100);
+
+}
